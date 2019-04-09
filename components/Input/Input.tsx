46ddfe33723af08d06cc42263b4c02fe7773b8a8
hileix <304192604@qq.com>
2019-04-09 13:35:26 +0800

    update

---
+++ components/Input/Input.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Input extends React.PureComponent {
+  static defaultProps = {};
+
+  static propTypes = {};
+
+  state = {};
+
+  render() {
+    return <Input />;
+  }
+}
