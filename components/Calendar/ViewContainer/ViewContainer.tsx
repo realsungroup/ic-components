c8fa68509dab9ec111129ace8fc3ccab4fd41b2a
邵志远 <yuanhjty@163.com>
2019-04-21 18:27:35 +0800

    fix: disable pan

--- components/Calendar/ViewContainer/ViewContainer.tsx
+++ components/Calendar/ViewContainer/ViewContainer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import debounce from 'lodash/debounce';
-import Hammer from 'hammerjs';
+// import debounce from 'lodash/debounce';
+// import Hammer from 'hammerjs';
 
 export default class ViewContainer extends React.Component<any, any> {
   static defaultProps = {
@@ -8,47 +8,47 @@ export default class ViewContainer extends React.Component<any, any> {
   };
 
   ref: any = React.createRef();
-  private hammer: any;
-  private contentColumnElement: Element;
-  private scrolling: boolean = false;
+  // private hammer: any;
+  // private contentColumnElement: Element;
+  // private scrolling: boolean = false;
 
   componentDidMount() {
-    const containerElement = this.ref.current;
-    this.contentColumnElement = containerElement.querySelector('.ic-day-time-line__content-column');
-    if (this.contentColumnElement) {
-      this.contentColumnElement.addEventListener('scroll', this.handleScroll);
-      this.contentColumnElement.addEventListener('touchend', this.handleScrollEnd);
-    }
-    this.hammer = new Hammer(containerElement);
-    this.hammer.get('pan').set({ direction: Hammer.DIRECTION_ALL });
-    this.hammer.on('pan', this.handlePan);
+    // const containerElement = this.ref.current;
+    // this.contentColumnElement = containerElement.querySelector('.ic-day-time-line__content-column');
+    // if (this.contentColumnElement) {
+    //   this.contentColumnElement.addEventListener('scroll', this.handleScroll);
+    //   this.contentColumnElement.addEventListener('touchend', this.handleScrollEnd);
+    // }
+    // this.hammer = new Hammer(containerElement, { domEvents: true });
+    // this.hammer.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+    // this.hammer.on('pan', this.handlePan);
   }
 
   componentWillUnmount() {
-    this.hammer.off('pan', this.handlePan);
-    if (this.contentColumnElement) {
-      this.contentColumnElement.removeEventListener('scroll', this.handleScroll);
-      this.contentColumnElement.removeEventListener('touchend', this.handleScrollEnd);
-    }
+    // this.hammer.off('pan', this.handlePan);
+    // if (this.contentColumnElement) {
+    //   this.contentColumnElement.removeEventListener('scroll', this.handleScroll);
+    //   this.contentColumnElement.removeEventListener('touchend', this.handleScrollEnd);
+    // }
   }
 
-  handleScroll = () => {
-    this.scrolling = true;
-  }
+  // handleScroll = () => {
+  //   this.scrolling = true;
+  // }
 
-  handleScrollEnd = () => {
-    setTimeout(() => {
-      this.scrolling = false;
-    }, 1000);
-  }
+  // handleScrollEnd = () => {
+  //   setTimeout(() => {
+  //     this.scrolling = false;
+  //   }, 1000);
+  // }
 
-  handlePan = debounce(e => {
-    if (this.scrolling) {
-      return;
-    }
+  // handlePan = debounce(e => {
+  //   if (this.scrolling) {
+  //     return;
+  //   }
 
-    this.props.onPan(e);
-  }, 300);
+  //   this.props.onPan(e);
+  // }, 300);
 
   render() {
     const { children, height } = this.props;
