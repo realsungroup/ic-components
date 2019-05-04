aae1b1f1558a94fbbad3ac92e925cd01107cbe73
hileix <304192604@qq.com>
2019-05-04 14:35:24 +0800

    chore:Mod Storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,77 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
 
 const req = require.context('../components', true, /\.stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+// Option defaults:
+addParameters({
+  options: {
+    /**
+     * show story component as full screen
+     * @type {Boolean}
+     */
+    isFullscreen: false,
+    /**
+     * display panel that shows a list of stories
+     * @type {Boolean}
+     */
+    showNav: true,
+    /**
+     * display panel that shows addon configurations
+     * @type {Boolean}
+     */
+    showPanel: false,
+    /**
+     * where to show the addon panel
+     * @type {('bottom'|'right')}
+     */
+    panelPosition: 'bottom',
+    /**
+     * sorts stories
+     * @type {Boolean}
+     */
+    sortStoriesByKind: false,
+    /**
+     * regex for finding the hierarchy separator
+     * @example:
+     *   null - turn off hierarchy
+     *   /\// - split by `/`
+     *   /\./ - split by `.`
+     *   /\/|\./ - split by `/` or `.`
+     * @type {Regex}
+     */
+    hierarchySeparator: /\/|\./,
+    /**
+     * regex for finding the hierarchy root separator
+     * @example:
+     *   null - turn off multiple hierarchy roots
+     *   /\|/ - split by `|`
+     * @type {Regex}
+     */
+    hierarchyRootSeparator: /\|/,
+    /**
+     * sidebar tree animations
+     * @type {Boolean}
+     */
+    sidebarAnimations: true,
+    /**
+     * enable/disable shortcuts
+     * @type {Boolean}
+     */
+    enableShortcuts: true,
+    /**
+     * show/hide tool bar
+     * @type {Boolean}
+     */
+    isToolshown: true,
+    /**
+     * theme storybook, see link below
+     */
+    theme: undefined,
+  },
+});
 
 configure(loadStories, module);
